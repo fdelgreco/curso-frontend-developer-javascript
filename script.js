38834a9b2841dc6061fa6d48167b5977692a3160
fdelgreco <fdelgreco92@gmail.com>
2023-02-24 18:33:30 -0300

    lal

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const tocarMail = document.querySelector (".navbar-email")
 const desktopMenu = document.querySelector (".desktop-menu")
 const menuHam = document.querySelector (".menu")
 const mobileMenu = document.querySelector (".mobile-menu")
+const carritoCompra = document.querySelector (".navbar-shopping-cart")
+const productDetail = document.querySelector (".product-detail")
 
 tocarMail.addEventListener ("click", toggleDesktopMenu)
+menuHam.addEventListener ("click", showMenu)
+carritoCompra.addEventListener ("click", showShopping)
 
 function toggleDesktopMenu () {
     console.log("hola");
@@ -11,8 +15,10 @@ function toggleDesktopMenu () {
 
 }
 
-menuHam.addEventListener ("click", showMenu)
-
 function showMenu () {
     mobileMenu.classList.toggle ("inactive")
+}
+
+function showShopping (){
+productDetail.classList.toggle ("inactive")
 }
